20c68fdd40023dfbce20f134cd19b95b69fb37e1
camiicode <dftp93@gmail.com>
2025-03-27 14:34:02 -0500

    Put all styles inside style tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    buildEnd() {
+      // Copia todo lo de _astro/ a assets/
+      cpSync('./dist/_astro', './dist/assets/', { recursive: true });
+    }
   },
   site: 'https://camiicode.github.io',
   base: '/dados-bot/',
+  build: {
+    format: 'directory'
+  }
 });
